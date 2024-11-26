547140786770e218756159dc41acc68f800f7a5b
Eklavya Agrawal <iamanswer123472@gmail.com>
2024-11-25 22:51:39 -0600

    good changes

---
+++ interface.py
@@ -0,0 +1,8 @@
+import EaseOfAccessCmds
+import FunctionalCalcs
+
+print("Copyright 2024, GPL v3.0|FREE TO DISTRIBUTE, USE, AND SELL, NO PROPRIETARY SOFTWARE, Riklik1984")
+print("for more information on copyright or licensing, please type 'info'")
+
+while True:
+    exit()
