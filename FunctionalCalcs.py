7746cbd07e887133eba6d54086889641e00090e7
Eklavya Agrawal <iamanswer123472@gmail.com>
2024-12-01 15:02:55 -0600

    delted DOA, added info to each file

---
+++ FunctionalCalcs.py
@@ -0,0 +1,7 @@
+#main calculator functions, may have EOA cmds if necessary (put most EOA cmds in interface directly then)
+
+def basicIntercept(mainInput):
+    try:
+        if mainInput == "*":
+            print("x * y")
+    except:
--- interface.py
+++ interface.py
@@ -1,8 +1,22 @@
-import EaseOfAccessCmds
+
 import FunctionalCalcs
+
+debug = False
+
+def debugPrint(systemIn):
+    if debug == True:
+        print(systemIn)
+
+
 
 print("Copyright 2024, GPL v3.0|FREE TO DISTRIBUTE, USE, AND SELL, NO PROPRIETARY SOFTWARE, Riklik1984")
 print("for more information on copyright or licensing, please type 'info'")
 
 while True:
-    exit()
+    try:
+        user_input = input("Please enter a command: ")
+        debugPrint(user_input)
+        
+    except KeyboardInterrupt:
+        print("Exiting...")
+        break
