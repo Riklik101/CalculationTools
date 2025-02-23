d96f43a11bd59952a76993ecc907427696b80053
Eklavya Agrawal <iamanswer123472@gmail.com>
2025-02-23 11:33:11 -0600

    Collatz, pip lib

--- interface.py
+++ interface.py
@@ -9,6 +9,7 @@ def debugPrint(systemIn):
 
 
 
+
 print("Copyright 2024, GPL v3.0|FREE TO DISTRIBUTE, USE, AND SELL, NO PROPRIETARY SOFTWARE, Riklik1984")
 print("for more information on copyright or licensing, please type 'info'")
 
