ee2ab6a1e40caa6e595634fa45d05cebb215d945
Eklavya Agrawal <iamanswer123472@gmail.com>
2025-03-11 13:18:06 -0500

    ahsing start

---
+++ basichash.py
@@ -0,0 +1,9 @@
+'''
+Deterministic: The same input always produces the same hash.
+Fixed Output Size: Regardless of input size, the output (hash) is of a fixed length.
+Fast Computation: The hash function should compute efficiently.
+Preimage Resistance: It should be computationally infeasible to reverse a hash to obtain the original input.
+Collision Resistance: Two different inputs should not produce the same hash.
+Avalanche Effect: A small change in input should result in a drastically different hash.
+'''
+
