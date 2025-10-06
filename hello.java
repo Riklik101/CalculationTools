ffa044e30c0e8d8aa168f1e926cc4140cf6db395
Eklavya Agrawal <iamanswer123472@gmail.com>
2025-10-05 22:15:53 -0500

    more work

--- hello.java
+++ hello.java
@@ -1,31 +1,49 @@
+import java.util.Scanner; // Import the Scanner class to read input
+//math is a built in library
+// java.lang is imported by default
+/* this is the structure for a multiline comment
+ * this is the second line of the comment
+ */
+
 public class hello {
     public static void main(String[] args) {
+
+
         System.out.println("Hello, World!");
         System.out.println("This is going to be a simple recap of what I am learning in compsci 1 rn");
 
+        //---------------start of learning-----------------
         double flo = 2.445;
         int x = 3;
-        try {
-            System.out.println(flo / 0);
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+        System.out.println(flo / 0);
+        // Dividing a float by 0 gives Infinity, not an error in Java.
+        System.out.println("5/2 (int division): " + (5 / 2)); // 2
+        System.out.println("Math.round(5/2): " + Math.round(5 / 2)); // 2
+        System.out.println("5.0/2 (double division): " + (5.0 / 2)); // 2.5
+        // Strings are objects in Java. Use Scanner for input.
+        System.out.println("Enter your name: ");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine(); // Read user input
+        System.out.println("Hello, " + name); // Output user input
+        for(int i=0;i<5;i++){
+            System.out.println(i);
         }
-        // Interestingly, this does not throw an error
-        // This is because in java, dividing a floating point number by 0 results in Infinity, not an error
+        //This differs from python as python uses "in" for loops
+        //java is much more strict with syntax
+        //for example
+        try {
+            int[] arr = {1, 2, 3};
+            System.out.println(arr[5]); // This will throw an ArrayIndexOutOfBoundsException
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+        try
+        {
+            int a = 5 / 0; // This will throw an ArithmeticException
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
 
-        // Now, some things that you remember from python that do NOT apply to java
-        // You must declare one variable as a double if you want it to divide as a double with another integer or other type
-        // Importantly, java does NOT round, it truncates
-        // So, if you want to round, you must use Math.round()
-        System.out.println("For example, 5/2 = " + (5 / 2)); // integer division, result is 2
-        System.out.println("But if you want to round, you must do Math.round(5/2) = " + Math.round(5 / 2)); // still 2, since 5/2 is int
-        // However, if you want to get a double result, you must declare one of the numbers as a double
-        System.out.println("So, 5.0/2 = " + (5.0 / 2)); // double division, result is 2.5
-        // Or, you can cast one of the numbers as a double
-
-        // Anyways, next up is strings. In java, strings are objects, not primitive types
-        // Input is not simple, however, you can use the Scanner class to get input from the user
-        // For example, you can do the following:
 
     }
 }
