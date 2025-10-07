77364f386981a8d75722e5fb088a227ad66b6ab9
Eklavya Agrawal <iamanswer123472@gmail.com>
2025-10-06 21:13:29 -0500

    skeleton code for calc

---
+++ Interface.java
@@ -0,0 +1,15 @@
+/*
+ * Interface.java
+ * Minimal entry-point that will use your JavaCalc class. Fill in the body to call your calculation tools.
+ *
+ * Keep this file in the same directory as JavaCalc.java (default package) so compilation is simple.
+ */
+
+public class Interface {
+    public static void main(String[] args) {
+        // TODO: create a JavaCalc instance and call your methods here
+        // Example:
+        // JavaCalc calc = new JavaCalc();
+        // System.out.println(calc.add(1, 2));
+    }
+}
---
+++ JavaCalc.java
@@ -0,0 +1,22 @@
+/*
+ * JavaCalc.java
+ * Skeleton for your calculation tools. Add methods, fields and constructors below.
+ * Keep this file in the top-level directory (no package) for easy compilation with other files in this workspace.
+ *
+ * TODO: implement calculation methods and any helper classes you need.
+ */
+
+public class JavaCalc {
+    // TODO: Add fields if needed
+
+    // Default constructor
+    public JavaCalc() {
+        // TODO: initialize fields if you add any
+    }
+
+    // Example method signatures you can implement; currently left empty on purpose.
+    // public int add(int a, int b) { /* implement me */ }
+    // public double divide(double a, double b) { /* implement me */ }
+
+    // Add your calculation methods below...
+}
